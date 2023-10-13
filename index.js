428d8cea8eb82e98ee1a815c7d5f4218dd8e90f5
AndresFLK <andrescrptycol@gmail.com>
2023-10-13 16:35:17 -0600

    Final project

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 var task = "";
 var taskList = [];
+const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,21 +19,29 @@ function addItem(req, res, next){
   };
   next();
 };
-
 app.use(addItem);
 
+
 app.get("/", (req, res) => {
-  
+    var dateTime = new Date();
+    var day = week[dateTime.getDay()];
+    var dayNumber = dateTime.getDate();
     res.render("index.ejs", {
-      day: new Date().getDate()
+      day: day,
+      dayNumber: dayNumber
     });
 });
 
 
 app.post("/submit", (req, res) => {
+  var dateTime = new Date();
+    var day = week[dateTime.getDay()];
+    var dayNumber = dateTime.getDate();
   console.log(taskList);
   res.render("index.ejs", {
-    items: taskList
+    items: taskList,
+    day: day,
+    dayNumber: dayNumber
   });
 });
 
